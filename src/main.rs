8eb9c12abb4ba026b1656ad60b444c85225a69ed
Nic0w <nico.broquet@gmail.com>
2020-10-17 09:21:30 +0200

    Adding gpx structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+extern crate serde;
+extern crate quick_xml;
+
+use serde::Deserialize;
+use quick_xml::de::{from_str, DeError};
+
+struct Elevation {
+    #[serde(rename = "$value")]
+    value: f32
+}
+
+struct Name {
+    #[serde(rename = "$value")]
+    value: String
+}
+
+struct TrackPoint {
+    lat: f64,
+    lon: f64,
+
+    #[serde(rename = "ele", default)]
+    elevations: Vec<Elevation>
+}
+
+struct TrackSegment {
+    #[serde(rename = "trkpt", default)]
+    points: Vec<TrackPoint>
+}
+
+struct Track {
+   name: Name,
+
+   #[serde(rename = "trkseg", default)]
+   segments: Vec<TrackSegment>
+}
+
+struct Metadata {
+    name: Name
+}
+
+struct Gpx {
+    creator: String,
+    version: String,
+
+    metadata: Metadata,
+    
+    #[serde(rename = "trk", default)]
+    tracks: Vec<Track>
+}
+
 fn main() {
     println!("Hello, world!");
 
